51402508d91284f1e067a2da7aaee3a08783b784
folex <0xdxdy@gmail.com>
2021-03-17 23:54:31 +0300

    revert url-downloader for workshop

---
+++ url-downloader/deploy_separate.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -euo pipefail
+
+./build.sh
+
+echo "Deploying storage"
+(
+  cd artifacts
+  fldist new_service --name "local_storage" --modules local_storage.wasm:local_storage.json
+)
+
+echo "\n\nDeploying curl"
+(
+  cd artifacts
+  fldist new_service --name "curl_adapter" --modules curl_adapter.wasm:curl_adapter.json
+)
