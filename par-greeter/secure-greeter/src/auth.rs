bff6358cc7b5581b98814fec1ae65ebadae9df91
boneyard93501 <4523011+boneyard93501@users.noreply.github.com>
2021-04-09 22:50:07 -0500

    init secure greeter

---
+++ par-greeter/secure-greeter/src/auth.rs
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2021 Fluence Labs Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use ::fluence;
+use fluence::{fce, CallParameters};
+
+pub fn is_owner() -> bool {
+    let meta = fluence::get_call_parameters();
+    let caller = meta.init_peer_id;
+    let owner = meta.service_creator_peer_id;
+
+    caller == owner
+}
+
+#[fce]
+pub fn am_i_owner() -> bool {
+    is_owner()
+}
