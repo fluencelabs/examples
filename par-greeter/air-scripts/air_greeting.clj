819ac68c5325bb8a1555218c9662e9c36adef24a
boneyard93501 <4523011+boneyard93501@users.noreply.github.com>
2021-04-18 15:35:02 -0500

    update projec

--- par-greeter/air-scripts/air_greeting.clj
+++ par-greeter/air-scripts/air_greeting.clj
@@ -1,10 +1,7 @@
 (xor
     (seq
-        (call relay (greeting_service  "greeting") [name] result)
+        (call relay (service  "greeting") [name greeter] result)
         (call %init_peer_id% (returnService "run") [result])
     )
-    (seq
-        (call relay (greeting_service  "greeting") [name] result)
-        (call %init_peer_id% (returnService "run") [%last_error%])
-    )
+    (call %init_peer_id% (returnService "run") [%last_error%])
 )
