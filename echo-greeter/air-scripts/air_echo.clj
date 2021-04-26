77a5732c1672fac303de6a0219348897468be4c9
boneyard93501 <4523011+boneyard93501@users.noreply.github.com>
2021-04-25 21:02:53 -0500

    update air scripts

--- echo-greeter/air-scripts/air_echo.clj
+++ echo-greeter/air-scripts/air_echo.clj
@@ -2,16 +2,16 @@
     (seq
         (seq
             (call relay ("op" "identity") [])
-            (call node (echo_service "echo") [names]  array_result)
+            (call relay (echo_service "echo") [names]  array_result)
         )
         (seq
             (call relay ("op" "identity") [])
+            ; (call %init_peer_id% (returnService "run") [array_result.$.[0]["echo"]])
             (call %init_peer_id% (returnService "run") [array_result])
         )
     )
     (seq
         (call relay ("op" "identity") [])
-        (call %init_peer_id% (returnService "run") ["XOR FAILED %last_error"])
+        (call %init_peer_id% (returnService "run") ["XOR FAILED: " %last_error%])
     )
-
 )
--- echo-greeter/air-scripts/air_greeting.clj
+++ echo-greeter/air-scripts/air_greeting.clj
@@ -1,6 +1,6 @@
 (xor
     (seq
-        (call relay (service  "greeting") [name greeter] result)
+        (call relay (greeting_service  "greeting") [name greeter] result)
         (call %init_peer_id% (returnService "run") [result])
     )
     (call %init_peer_id% (returnService "run") [%last_error%])
