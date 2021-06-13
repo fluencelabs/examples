256aae9c0f657cf9bee452eeddd2c3a7d7b3b88c
folex <0xdxdy@gmail.com>
2021-06-13 23:54:37 +0300

    Add curl_template to CI (#7)

--- curl_template/deploy.sh
+++ curl_template/deploy.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -o errexit -o nounset -o pipefail
 
-# check `fcli` and `fldist` tools are installed or install them
+if [[ -z "${NODE:-}" ]]; then
+    NODE_ADDR=""
+else
+    NODE_ADDR="--node-addr $NODE"
+fi
+
+# check `marine` and `fldist` tools are installed or install them
 (command -v marine || cargo install marine) >/dev/null
 (command -v fldist || npm install -g @fluencelabs/fldist) >/dev/null
 
@@ -17,4 +23,5 @@ test -f "$WASM" || echo >&2 "Couldn't find $WASM"
 
 # create a service from that .wasm
 CONFIG="$(pwd)/backend/BackendConfig.json"
-fldist new_service --modules "$WASM:$CONFIG" --name curl_template
+SERVICE_ID=$(fldist new_service $NODE_ADDR --modules "$WASM:$CONFIG" --name call_parameters | head -n1 | sed -e 's/service id: //')
+echo $SERVICE_ID
