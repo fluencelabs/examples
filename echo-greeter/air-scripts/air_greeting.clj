a66ccd6514c0447ad8a0a77fa5b333263984a14e
boneyard93501 <4523011+boneyard93501@users.noreply.github.com>
2021-06-16 18:35:08 -0500

    add compiled aqua, remove air-scripts

--- echo-greeter/air-scripts/air_greeting.clj
+++
@@ -1,7 +0,0 @@
-(xor
-    (seq
-        (call relay (greeting_service  "greeting") [name greeter] result)
-        (call %init_peer_id% (returnService "run") [result])
-    )
-    (call %init_peer_id% (returnService "run") [%last_error%])
-)
