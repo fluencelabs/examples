531f715dcae44b6f67cc9735a0514b56acfab7c8
folex <0xdxdy@gmail.com>
2021-07-20 18:20:43 +0300

    remove upload mock

--- getting-started/ipfs-execution/web/src/App.tsx
+++ getting-started/ipfs-execution/web/src/App.tsx
@@ -20,39 +20,39 @@ function App() {
 
   const [peerIdInput, setPeerIdInput] = useState<string>("");
   const [relayPeerIdInput, setRelayPeerIdInput] = useState<string>("");
-  const [wasm, setWasm] = useState<string | null>(null);
+  const [wasm, setWasm] = useState<string | null>("QmVg9EnanAbwTuEqjjuc1R2uf3AdtEkrNagSifQMkHfyNU");
   const [rpcAddr, setRpcAddr] = useState<string | null>("");
   const [fileCID, setFileCID] = useState<string>("");
   const [fileSize, setFileSize] = useState<number | null>(null);
 
   const isConnected = client !== null;
-  const uploaded = wasm !== null;
+  const gotRpcAddr = rpcAddr !== null;
   const deployed = serviceId !== null;
 
-  const connect = (relayPeerId: string) => {
-    createClient(relayPeerId)
-      .then((client) => {
-          setClient(client);
-      })
-      .catch((err) => console.log("Client initialization failed", err));
+  const connect = async (relayPeerId: string) => {
+    try {
+      let client = await createClient(relayPeerId);
+      setClient(client);
+      await getRpcAddr(client);
+    } catch (err) {
+      console.log("Client initialization failed", err);
+    }
   };
 
-  const uploadWasm = async () => {
+  const getRpcAddr = async (client: FluenceClient) => {
     if (client === null) {
+      console.log("getRpcAddr client is null");
       return;
     }
-    // let {file, rpcAddr } = await provideFile(globSource("../service/artifacts/process_files.wasm"), client)
-    // let { file, rpcAddr } = await provideFile(urlSource("https://transfer.sh/13jim4O/process_files.wasm"), client);
-    let result = await get_external_api_multiaddr(client, client.relayPeerId!);
-    let rpcAddr = result.multiaddr;
-    console.dir(rpcAddr);
 
-    setWasm("QmVg9EnanAbwTuEqjjuc1R2uf3AdtEkrNagSifQMkHfyNU");
+    let result = await get_external_api_multiaddr(client, client.relayPeerId!);
+    console.log("getRpcAddr result", result);
+    let rpcAddr = result.multiaddr;
     setRpcAddr(rpcAddr);
-    // QmVg9EnanAbwTuEqjjuc1R2uf3AdtEkrNagSifQMkHfyNU
-  };
+  }
 
   const deployService = async () => {
+    console.log("wasm %s rpcAddr %s", wasm, rpcAddr);
     if (client === null || wasm === null || rpcAddr === null) {
       return;
     }
@@ -83,7 +83,7 @@ function App() {
     setServiceId(null);
   };
 
-    console.log("isConnected, uploaded, deployed\n", isConnected, uploaded, deployed);
+    console.log("isConnected gotRpcAddr deployed\n", isConnected, gotRpcAddr, deployed);
 
     if (!isConnected) {
       return (<div className="App">
@@ -98,7 +98,7 @@ function App() {
               {relayNodes.map((x) => (
                 <li key={x.peerId}>
                   <span className="mono">{x.peerId}</span>
-                  <button className="btn" onClick={() => connect(x.multiaddr)}>
+                  <button className="btn" onClick={async () => await connect(x.multiaddr)}>
                     Connect
                   </button>
                 </li>
@@ -108,7 +108,7 @@ function App() {
           </div>
           </div>
       );
-    } else if (isConnected && !uploaded && !deployed) {
+    } else if (isConnected && gotRpcAddr && !deployed) {
       return (
         <div className="App">
           <header>
@@ -143,63 +143,33 @@ function App() {
                   </button>
                 </td>
               </tr>
+              <tr>
+                <td className="bold">IPFS RPC:</td>
+                <td className="mono">{rpcAddr?.substring(0, 49) + "..."}</td>
+                <td>
+                  <button
+                    className="btn-clipboard"
+                    onClick={() => copyToClipboard(rpcAddr!)}
+                  >
+                    <i className="gg-clipboard"></i>
+                  </button>
+                </td>
+              </tr>
             </table>
             <div>
-              <h2>Upload .wasm module!</h2>
-              <p className="p">
-                You will upload 'process_files.wasm' module to IPFS.
-              </p>
               <div className="row">
-                <button className="btn btn-hello" onClick={uploadWasm}>
-                  upload .wasm
-                </button>
+                <h2>Set process_files.wasm module CID</h2>
+                <p className="p">
+                  To deploy a service, specify CID of WebAssembly module.
+                </p>
+                <input
+                  className="input"
+                  type="text"
+                  onChange={(e) => setWasm(e.target.value)}
+                  value={wasm!}
+                />
               </div>
             </div>
-          </>
-          </div>
-        </div>
-      )
-    } else if (uploaded && !deployed) {
-      console.log("uploaded!", wasm);
-      return (
-        <div className="App">
-          <header>
-            <img src={logo} className="logo" alt="logo" />
-          </header>
-
-          <div className="content">
-            <>
-            <h1>Uploaded</h1>
-            <table>
-              <tr>
-                <td className="bold">Peer id:</td>
-                <td className="mono">{client!.selfPeerId}</td>
-                <td>
-                  <button
-                    className="btn-clipboard"
-                    onClick={() => copyToClipboard(client!.selfPeerId)}
-                  >
-                    <i className="gg-clipboard"></i>
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td className="bold">Relay peer id:</td>
-                <td className="mono">{client!.relayPeerId}</td>
-                <td>
-                  <button
-                    className="btn-clipboard"
-                    onClick={() => copyToClipboard(client!.relayPeerId!)}
-                  >
-                    <i className="gg-clipboard"></i>
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td className="bold">process_files.wasm CID:</td>
-                <td className="mono">{wasm}</td>
-              </tr>
-            </table>
             <div>
               <h2>Deploy ProcessFiles service</h2>
               <p className="p">
@@ -215,9 +185,9 @@ function App() {
               </div>
             </div>
           </>
+          </div>
         </div>
-        </div>
-      );
+      )
     } else if (deployed) {
       return (
         <div className="App">
@@ -256,6 +226,14 @@ function App() {
               <tr>
                 <td className="bold">process_files.wasm CID:</td>
                 <td className="mono">{wasm}</td>
+                <td>
+                  <button
+                    className="btn-clipboard"
+                    onClick={() => copyToClipboard(wasm!)}
+                  >
+                    <i className="gg-clipboard"></i>
+                  </button>
+                </td>
               </tr>
               <tr>
                 <td className="bold">ProcessFiles service ID:</td>
@@ -310,7 +288,7 @@ function App() {
 
           <div className="content">
           <>
-            <h2>Unimplemented! isConnected {isConnected} uploaded {uploaded} deployed {deployed} wasm {wasm} </h2>
+            <h2>Unimplemented! isConnected {isConnected} deployed {deployed} wasm {wasm} </h2>
           </>
         </div>
         </div>
