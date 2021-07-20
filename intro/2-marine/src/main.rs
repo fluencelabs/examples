891a09253a83b619c35dca9bfe972a8d3c519d92
Pavel Murygin <pavel.murygin@gmail.com>
2021-07-20 13:58:10 +0300

    Add marine part to the intro examples

---
+++ intro/2-marine/src/main.rs
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2021 Fluence Labs Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use marine_rs_sdk::marine;
+use marine_rs_sdk::module_manifest;
+
+module_manifest!();
+
+#[marine]
+pub struct HelloComputation {
+    pub msg: String,
+    pub reply: String,
+}
+
+pub fn main() {}
+
+#[marine]
+pub fn hello_world(from: String) -> HelloComputation {
+    HelloComputation {
+        msg: format!("Hello from: \n{}", from),
+        reply: format!("Hello back to you, \n{}", from)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use marine_rs_sdk_test::marine_test;
+
+    #[marine_test(config_path = "../Config.toml", modules_dir = "../artifacts")]
+    fn empty_string() {
+        let actual = greeting.greeting(String::new());
+        assert_eq!(actual, "Hi, ");
+    }
+
+    #[marine_test(config_path = "../Config.toml", modules_dir = "../artifacts")]
+    fn non_empty_string() {
+        let actual = greeting.greeting("name".to_string());
+        assert_eq!(actual, "Hi, name");
+    }
+}
