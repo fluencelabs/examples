df71dbc7635c7d8dcc30bb8742c8081fb2c6c3a1
folex <0xdxdy@gmail.com>
2021-07-20 17:38:12 +0300

    mock uploading

--- getting-started/ipfs-execution/web/src/App.tsx
+++ getting-started/ipfs-execution/web/src/App.tsx
@@ -3,10 +3,12 @@ import logo from "./logo.svg";
 import "./App.scss";
 
 import { createClient, FluenceClient } from "@fluencelabs/fluence";
-import { krasnodar } from "@fluencelabs/fluence-network-environment";
-import { deploy_service, get_file_size, remove_service, globSource, provideFile, CID, urlSource } from "@fluencelabs/ipfs-execution";
+import { get_external_api_multiaddr } from "@fluencelabs/aqua-ipfs";
+import { stage } from "@fluencelabs/fluence-network-environment";
+import { deploy_service, get_file_size, remove_service, provideFile } from "@fluencelabs/ipfs-execution";
+const { create, globSource, urlSource, CID } = require('ipfs-http-client');
 
-const relayNodes = [krasnodar[0], krasnodar[1], krasnodar[2]];
+const relayNodes = [stage[0], stage[1], stage[2]];
 
 const copyToClipboard = (text: string) => {
   navigator.clipboard.writeText(text);
@@ -18,7 +20,7 @@ function App() {
 
   const [peerIdInput, setPeerIdInput] = useState<string>("");
   const [relayPeerIdInput, setRelayPeerIdInput] = useState<string>("");
-  const [wasm, setWasm] = useState<typeof CID | null>(null);
+  const [wasm, setWasm] = useState<string | null>(null);
   const [rpcAddr, setRpcAddr] = useState<string | null>("");
   const [fileUrl, setFileUrl] = useState<string>("");
   const [fileSize, setFileSize] = useState<number | null>(null);
@@ -39,9 +41,15 @@ function App() {
     if (client === null) {
       return;
     }
-    let {file, rpcAddr } = await provideFile(globSource("../service/artifacts/process_files.wasm"), client)
-    setWasm(file);
+    // let {file, rpcAddr } = await provideFile(globSource("../service/artifacts/process_files.wasm"), client)
+    // let { file, rpcAddr } = await provideFile(urlSource("https://transfer.sh/13jim4O/process_files.wasm"), client);
+    let result = await get_external_api_multiaddr(client, client.relayPeerId!);
+    let rpcAddr = result.multiaddr;
+    console.dir(rpcAddr);
+
+    setWasm("QmVg9EnanAbwTuEqjjuc1R2uf3AdtEkrNagSifQMkHfyNU");
     setRpcAddr(rpcAddr);
+    // QmVg9EnanAbwTuEqjjuc1R2uf3AdtEkrNagSifQMkHfyNU
   };
 
   const deployService = async () => {
@@ -50,7 +58,7 @@ function App() {
     }
     let service_id = await deploy_service(
         client, 
-        client.relayPeerId!, wasm.cid.toString(), rpcAddr, 
+        client.relayPeerId!, wasm, rpcAddr, 
         (msg, value) => console.log(msg, value),
         { ttl: 10000 }
     );
@@ -76,7 +84,7 @@ function App() {
     setServiceId(null);
   };
 
-    console.log(isConnected, uploaded, deployed);
+    console.log("isConnected, uploaded, deployed\n", isConnected, uploaded, deployed);
 
     if (!isConnected) {
       return (<div className="App">
@@ -153,6 +161,7 @@ function App() {
         </div>
       )
     } else if (uploaded && !deployed) {
+      console.log("uploaded!", wasm);
       return (
         <div className="App">
           <header>
@@ -189,21 +198,20 @@ function App() {
               </tr>
               <tr>
                 <td className="bold">CID:</td>
-                <td className="mono">{wasm.cid.to_string()}</td>
-              </tr>
-              <tr>
-                <td className="bold">Size:</td>
-                <td className="mono">{wasm.size}</td>
+                <td className="mono">{wasm}</td>
               </tr>
             </table>
             <div>
-              <h2>Upload .wasm module!</h2>
+              <h2>Deploy ProcessFiles service</h2>
               <p className="p">
-                You will upload 'process_files.wasm' module to IPFS.
+                process_files.wasm will be downloaded to the Fluence node,
+                and then a service will be dynamically created from it! 
+                
+                After that, you will be able to use that service to get sizes of IPFS files!
               </p>
               <div className="row">
-                <button className="btn btn-hello" onClick={uploadWasm}>
-                  upload .wasm
+                <button className="btn btn-hello" onClick={deployService}>
+                  deploy service
                 </button>
               </div>
             </div>
@@ -213,9 +221,7 @@ function App() {
       );
 
       /*
-      , then download it to the Fluence node,
-                and dynamically create a service from it! 
-                After that, you will be able to use that service to sizes of IPFS files!
+      , then 
 
               <div className="row">
                 <label className="label bold">Peer id</label>
