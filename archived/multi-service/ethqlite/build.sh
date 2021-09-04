04614a856aee4b68c304f67d25ffbffbbc320781
boneyard93501 <4523011+boneyard93501@users.noreply.github.com>
2021-09-04 14:34:28 -0500

    update sqlite version

--- archived/multi-service/ethqlite/build.sh
+++ archived/multi-service/ethqlite/build.sh
@@ -7,5 +7,5 @@ marine build --release
 
 rm -f artifacts/*.wasm
 cp target/wasm32-wasi/release/ethqlite.wasm artifacts/
-wget https://github.com/fluencelabs/sqlite/releases/download/v0.14.0_w/sqlite3.wasm
+wget https://github.com/fluencelabs/sqlite/releases/download/v0.15.0_w/sqlite3.wasm
 mv sqlite3.wasm artifacts/
