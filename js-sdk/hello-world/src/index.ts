69b33158da4c050665066b70c84248dd4bcd032c
Pavel Murygin <pavel.murygin@gmail.com>
2021-09-06 12:28:06 +0300

    Putting code for hello-world example under source control

---
+++ js-sdk/hello-world/src/index.ts
@@ -0,0 +1,19 @@
+import { FluencePeer, randomPeerId } from "@fluencelabs/fluence";
+import { krasnodar } from "@fluencelabs/fluence-network-environment";
+import { registerHelloWorld, sayHello } from "./_aqua/hello-world";
+
+async function main() {
+  await FluencePeer.default.init();
+
+  registerHelloWorld({
+    hello: async (str) => {
+      console.log(str);
+    },
+  });
+
+  await sayHello();
+
+  await FluencePeer.default.uninit();
+}
+
+main();
