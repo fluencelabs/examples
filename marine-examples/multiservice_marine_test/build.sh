c83afdbf9d6aa6a4ce121ef258c3f7a6b110df41
Valery Antopol <valery.antopol@gmail.com>
2021-10-11 17:08:00 +0300

    Fix main.rs, add common build script

---
+++ marine-examples/multiservice_marine_test/build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+(cd producer; ./build.sh)
+(cd consumer; ./build.sh)
--- marine-examples/multiservice_marine_test/src/main.rs
+++ marine-examples/multiservice_marine_test/src/main.rs
@@ -5,7 +5,7 @@ mod tests {
     use marine_rs_sdk_test::marine_test;
     #[marine_test(
         producer(
-            config_path = "../producer/Config.toml",
+            config_path = "../producer/Config.toml", // <- 1
             modules_dir = "../producer/artifacts"
         ),
         consumer(
@@ -16,13 +16,12 @@ mod tests {
     fn test() {
         let mut producer = marine_test_env::producer::ServiceInterface::new();
         let mut consumer = marine_test_env::consumer::ServiceInterface::new();
-        let input = marine_test_env::producer::modules::producer::Input {
+        let input = marine_test_env::producer::Input {
             first_name: String::from("John"),
             last_name: String::from("Doe"),
         };
-        let data = producer.modules.producer.produce(input);
-        let consumer_data = marine_test_env::consumer::modules::consumer::Data { name: data.name };
-        let result = consumer.modules.consumer.consume(consumer_data);
+        let data = producer.produce(input);
+        let result = consumer.consume(data);
         assert_eq!(result, "John Doe")
     }
 }
