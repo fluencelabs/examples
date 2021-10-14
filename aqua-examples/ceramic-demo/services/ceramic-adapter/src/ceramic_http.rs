81371c6bf8f5a07ee798c55029cf6e59a4cbc200
boneyard93501 <4523011+boneyard93501@users.noreply.github.com>
2021-10-14 10:03:35 -0500

    update dirs

--- aqua-examples/ceramic-demo/services/ceramic-adapter/src/ceramic_http.rs
+++ aqua-examples/ceramic-demo/services/ceramic-adapter/src/ceramic_http.rs
@@ -1,16 +1,25 @@
 use marine_rs_sdk::{marine, MountedBinaryResult};
 
+// source: https://developers.ceramic.network/build/http/api/
 static API: &str = "api";
 static VERSION: &str = "v0";
 static STREAM: &str = "streams";
 
-fn url_maker(host: String, port: u32) -> String {
-    format!("{}:{}/{}/{}/{}", host, port, API, VERSION, STREAM)
+fn url_maker(host: String, port: u32, arg: String) -> String {
+    format!(
+        "{}:{}/{}/{}/{}",
+        host,
+        port,
+        API,
+        VERSION,
+        arg.to_uppercase()
+    )
 }
 
 #[marine]
-pub fn http_state(url: String, port: u32, payload: String) {
-    let url = url_maker(url, port);
+pub fn http_streams(url: String, port: u32, payload: String) {
+    // curl http://localhost:7007/api/v0/streams/kjzl6cwe1jw147r7878h32yazawcll6bxe5v92348cxitif6cota91qp68grbhm
+    let url = url_maker(url, port, "streams");
     let cmd = vec![url, "GET".to_string()];
     let response = curl_request(cmd);
 }
