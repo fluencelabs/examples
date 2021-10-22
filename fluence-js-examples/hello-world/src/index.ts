ad8e18f01cf77e14eaa049d38c2c5ad349016326
Pavel <Pavel.Murygin@gmail.com>
2021-10-22 09:41:48 +0300

    Update fluence-js to 14.2 and aqua to 0.4.0-235 (#33)

--- fluence-js-examples/hello-world/src/index.ts
+++ fluence-js-examples/hello-world/src/index.ts
@@ -4,18 +4,27 @@ import {
   registerHelloWorld,
   sayHello,
   getRelayTime,
+  tellFortune,
 } from "./_aqua/hello-world";
 
 async function main() {
   await Fluence.start({ connectTo: krasnodar[0] });
 
   registerHelloWorld({
-    hello: async (str) => {
+    hello: (str) => {
       console.log(str);
+    },
+    getFortune: async () => {
+      await new Promise((resolve) => {
+        setTimeout(resolve, 1000);
+      });
+      return "Wealth awaits you very soon.";
     },
   });
 
   await sayHello();
+
+  console.log(await tellFortune());
 
   const relayTime = await getRelayTime();
 
