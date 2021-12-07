8103e1db57d8f67c09bdb3f71adfaf79b6bef7c0
folex <0xdxdy@gmail.com>
2021-12-07 12:49:22 +0300

    Add 'tcp' to multiaddr

--- near-examples/near-signing-node/src/index.ts
+++ near-examples/near-signing-node/src/index.ts
@@ -171,7 +171,7 @@ async function main() {
         // connectTo: krasnodar[5],
 
         connectTo: {
-            multiaddr: "/ip4/127.0.0.1/9990/ws/p2p/12D3KooWHBG9oaVx4i3vi6c1rSBUm7MLBmyGmmbHoZ23pmjDCnvK",
+            multiaddr: "/ip4/127.0.0.1/tcp/9990/ws/p2p/12D3KooWHBG9oaVx4i3vi6c1rSBUm7MLBmyGmmbHoZ23pmjDCnvK",
             peerId: "12D3KooWHBG9oaVx4i3vi6c1rSBUm7MLBmyGmmbHoZ23pmjDCnvK"
         },
 
