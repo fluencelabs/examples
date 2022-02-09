e34e15afa8c78c9b7c1ff76441d48e2f340fd16f
Pavel Murygin <Pavel.Murygin@gmail.com>
2022-02-09 15:14:41 +0300

    Fix FluenceJS node example

---
+++ fluence-js-examples/node-example/run_calculation.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+npx aqua run --input ./aqua/demo-calculation.aqua --func 'demoCalculation()' --addr /dns4/stage.fluence.dev/tcp/19001/wss/p2p/12D3KooWHCJbJKGDfCgHSoCuK9q4STyRnVveqLoXAPBbXHTZx9Cv 
