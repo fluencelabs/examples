f36f92297a4865a9cd1632585cd9eba36c06f6ac
Pavel Murygin <Pavel.Murygin@gmail.com>
2022-02-15 19:37:35 +0300

    hello-world

--- fluence-js-examples/hello-world/src/index.ts
+++ fluence-js-examples/hello-world/src/index.ts
@@ -1,36 +1,3 @@
-import { Fluence } from "@fluencelabs/fluence";
-import { krasnodar } from "@fluencelabs/fluence-network-environment";
-import {
-  registerHelloWorld,
-  sayHello,
-  getRelayTime,
-  tellFortune,
-} from "./_aqua/hello-world";
-
-async function main() {
-  await Fluence.start({ connectTo: krasnodar[0] });
-
-  registerHelloWorld({
-    hello: (str) => {
-      console.log(str);
-    },
-    getFortune: async () => {
-      await new Promise((resolve) => {
-        setTimeout(resolve, 1000);
-      });
-      return "Wealth awaits you very soon.";
-    },
-  });
-
-  await sayHello();
-
-  console.log(await tellFortune());
-
-  const relayTime = await getRelayTime();
-
-  console.log("The relay time is: ", new Date(relayTime).toLocaleString());
-
-  await Fluence.stop();
-}
+import { main } from './main';
 
 main();
