b4f1f1c052d2061e2129155e4b690d8ef5efe2db
Pavel Murygin <Pavel.Murygin@gmail.com>
2022-02-15 23:37:04 +0300

    node example

---
+++ fluence-js-examples/node-example/src/__test__/test.spec.ts
@@ -0,0 +1,12 @@
+import { runServer } from '../main';
+import { demoCalculation } from '../_aqua/demo-calculation';
+
+describe('smoke test', () => {
+    it('should work', async () => {
+        await runServer();
+
+        const res = await demoCalculation();
+
+        expect(res).toBe(7);
+    });
+});
