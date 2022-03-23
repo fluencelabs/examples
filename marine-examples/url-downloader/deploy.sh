97b87090b267ab804847b150a67fadb5d1b334d2
boneyard93501 <4523011+boneyard93501@users.noreply.github.com>
2022-03-23 13:45:30 -0500

    update path

--- marine-examples/url-downloader/deploy.sh
+++ marine-examples/url-downloader/deploy.sh
@@ -5,8 +5,8 @@ set -o errexit -o nounset -o pipefail
 ./build.sh
 
 (
-  cd artifacts
-  aqua remote deploy_service_service \
+
+ aqua remote deploy_service \
        --addr /dns4/kras-05.fluence.dev/tcp/19001/wss/p2p/12D3KooWCMr9mU894i8JXAFqpgoFtx6qnV1LFPSfVc3Y34N4h4LS \
        --data-path deployment_cfg.json \
        --service url-downloader
--- marine-examples/url-downloader/deploy_separate.sh
+++ marine-examples/url-downloader/deploy_separate.sh
@@ -5,7 +5,6 @@ set -o errexit -o nounset -o pipefail
 
 echo "Deploying storage"
 (
-  cd artifacts
   aqua remote deploy_service \
        --addr /dns4/kras-05.fluence.dev/tcp/19001/wss/p2p/12D3KooWCMr9mU894i8JXAFqpgoFtx6qnV1LFPSfVc3Y34N4h4LS \
        --data-path deployment_cfg.json \
@@ -14,7 +13,6 @@ echo "Deploying storage"
 
 echo "\n\nDeploying curl"
 (
-  cd artifacts
   aqua remote deploy_service \
       --addr /dns4/kras-05.fluence.dev/tcp/19001/wss/p2p/12D3KooWCMr9mU894i8JXAFqpgoFtx6qnV1LFPSfVc3Y34N4h4LS \
       --data-path deployment_cfg.json \
