e082a5442ca4b37edd7aa68b1e2edcf61c4f49f8
Pavel Murygin <Pavel.Murygin@gmail.com>
2022-04-14 00:31:33 +0300

    FluenceJS: fix test in nodejs example

--- fluence-js-examples/node-example/src/__test__/test.spec.ts
+++ fluence-js-examples/node-example/src/__test__/test.spec.ts
@@ -3,11 +3,14 @@ import { demoCalculation } from '../_aqua/demo-calculation';
 
 describe('smoke test', () => {
     it('should work', async () => {
-        await runServer();
+        try {
+            await runServer();
 
-        const res = await demoCalculation();
-        await justStop();
+            const res = await demoCalculation();
 
-        expect(res).toBe(7);
+            expect(res).toBe(7);
+        } finally {
+            await justStop();
+        }
     }, 15000);
 });
