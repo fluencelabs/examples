8186ad7c118f660c9f42868ab17c78d46c07e5d2
igor <dev@igor.sh>
2022-07-01 19:37:20 +0200

    fix for deployment: added the sk param support

--- quickstart/5-oracle-service/scripts/deploy.sh
+++ quickstart/5-oracle-service/scripts/deploy.sh
@@ -1,8 +1,30 @@
 #!/usr/bin/env bash -o errexit -o nounset -o pipefail
 
+function is_installed {
+      if command -v "$1" >/dev/null 2>&1; then
+         echo "${1} is installed"
+      else
+         echo "${1} not found, please install it"
+         echo "exiting..."
+         exit 1
+      fi
+}
+
+is_installed jq
+is_installed aqua
+
+echo "generating keys..."
+
+SK=$(aqua key create | jq -r ."secretKey")
+
+echo "deploying the service..."
+
 aqua remote deploy_service \
-     --addr /dns4/kras-04.fluence.dev/tcp/19001/wss/p2p/12D3KooWFEwNWcHqi9rtsmDhsYcDbRUCDXH84RC4FW6UfsFWaoHi \
+     --addr /dns4/kras-09.fluence.dev/tcp/19001/wss/p2p/12D3KooWD7CvsYcpF9HE9CCV9aY3SJ317tkXVykjtZnht2EbzDPm \
      --config-path configs/ts_oracle_deploy_cfg.json \
+     --sk ${SK} \
      --service ts-oracle \
 >>\
 deployed_service.data
+
+tail -4 deployed_service.data
