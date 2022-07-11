d393905549cb33e8ce23735464a7cb0156218247
boneyard93501 <4523011+boneyard93501@users.noreply.github.com>
2022-07-11 13:35:18 -0500

    rm old service dirs

--- aqua-examples/decentralized-blockchain-gateway/utilities/scripts/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash -o errexit -o nounset -o pipefail
-cargo update --aggressive
-
-mkdir -p artifacts
-rm -f artifacts/*.wasm
-marine build --release
-cp target/wasm32-wasi/release/utilities.wasm artifacts/
