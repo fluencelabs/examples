11c72d8e6b0b87b7b32b6ae6173e2964630c53e4
boneyard93501 <4523011+boneyard93501@users.noreply.github.com>
2022-07-11 19:25:34 -0500

    add ipfs adapter

--- aqua-examples/decentralized-blockchain-gateway/scripts/build.sh
+++ aqua-examples/decentralized-blockchain-gateway/scripts/build.sh
@@ -10,6 +10,11 @@ cargo update --aggressive
 marine build --release
 cp target/wasm32-wasi/release/curl_adapter.wasm ../../artifacts/
 
+cd ../ipfs-adapter
+cargo update --aggressive
+marine build --release
+cp target/wasm32-wasi/release/ipfs_adapter.wasm ../../artifacts/
+
 cd ../multi-provider-query
 cargo update --aggressive
 marine build --release
@@ -19,5 +24,15 @@ cd ../simple-quorum
 cargo update --aggressive
 marine build --release
 cp target/wasm32-wasi/release/simple_quorum.wasm ../../artifacts/
+
+cd ../ipfs-cli
+cargo update --aggressive
+marine build --release
+cp target/wasm32-wasi/release/ipfs_cli.wasm ../../artifacts/
+
+cd ../utilities
+cargo update --aggressive
+marine build --release
+cp target/wasm32-wasi/release/utilities.wasm ../../artifacts/
 
 cd ..
