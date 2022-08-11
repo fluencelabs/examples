4451babf1228b23713d96e80572685db6a920e65
igor <dev@igor.sh>
2022-08-11 15:51:00 +0200

    removed unnecessary files

--- aqua-examples/near-integration/services/near-adapter/modules/near-rpc-services/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# This script builds project to Wasm and puts all created binary Wasm module into the artifacts dir
-marine build --release
-
-rm artifacts/* || true
-mkdir -p artifacts
-
-cp ./target/wasm32-wasi/release/near_rpc_services.wasm artifacts/
