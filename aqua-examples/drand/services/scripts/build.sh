cdb9d1ca7b7c9db2bad09149a2937ec950cc7890
boneyard93501 <4523011+boneyard93501@users.noreply.github.com>
2022-11-29 18:27:20 -0600

    add scripts, artifacts

---
+++ aqua-examples/drand/services/scripts/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash -o errexit -o nounset -o pipefail
+
+mkdir -p artifacts
+rm -f artifacts/*.wasm
+
+cd curl_adapter
+marine build --release
+cp target/wasm32-wasi/release/curl_adapter.wasm ../artifacts/
+
+cd ../drand
+marine build --release
+cp target/wasm32-wasi/release/drand.wasm ../artifacts/
+
+cd ..
