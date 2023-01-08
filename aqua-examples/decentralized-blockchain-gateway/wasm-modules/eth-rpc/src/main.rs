a27eed7f84388ecb51ba6b6aea58806acba83d03
folex <0xdxdy@gmail.com>
2023-01-09 00:31:33 +0700

    back to MountedBinaryResult

--- aqua-examples/decentralized-blockchain-gateway/wasm-modules/eth-rpc/src/main.rs
+++ aqua-examples/decentralized-blockchain-gateway/wasm-modules/eth-rpc/src/main.rs
@@ -2,14 +2,13 @@ use jsonrpc_core::types::request::Call;
 use serde_json::json;
 use serde_json::Value;
 use tokio::runtime::Builder;
-use web3::{RequestId, Transport};
 use web3::futures::future::BoxFuture;
 use web3::helpers::CallFuture;
 use web3::types::Address;
+use web3::{RequestId, Transport};
 
-use marine_rs_sdk::marine;
-use marine_rs_sdk::MountedBinaryResult;
 use marine_rs_sdk::module_manifest;
+use marine_rs_sdk::{marine, MountedBinaryResult};
 
 module_manifest!();
 
@@ -26,8 +25,51 @@ impl Transport for Dummy {
         (1, request)
     }
 
-    fn send(&self, _: RequestId, _: Call) -> Self::Out {
-        Box::pin(async { Ok(json!(["0x407d73d8a49eeb85d32cf465507dd71d507100c1"])) })
+    fn send(&self, _: RequestId, call: Call) -> Self::Out {
+        if let Call::MethodCall(call) = call {
+            /*
+            curl --request POST \
+                 --url https://eth-mainnet.g.alchemy.com/v2/P8ZvwJbYKvGEdGFgIL7nsuwS18BKCYlR \
+                 --header 'accept: application/json' \
+                 --header 'content-type: application/json' \
+                 --data '
+            {
+                 "id": 1,
+                 "jsonrpc": "2.0",
+                 "method": "eth_accounts"
+            }
+            '
+            */
+            Box::pin(async move {
+                let json = json!(call).to_string();
+                let args = vec![
+                    "--request",
+                    "POST",
+                    "--url",
+                    "https://eth-mainnet.g.alchemy.com/v2/P8ZvwJbYKvGEdGFgIL7nsuwS18BKCYlR",
+                    "--header",
+                    "accept: application/json",
+                    "--header",
+                    "content-type: application/json",
+                    "--data",
+                    json.as_str(),
+                ];
+                let args = args.into_iter().map(|s| s.to_string()).collect();
+                let response = curl_request(args);
+                println!(
+                    "response is: \nstdout {}\nstderr: {}",
+                    String::from_utf8(response.stdout.clone()).unwrap(),
+                    String::from_utf8(response.stderr.clone()).unwrap()
+                );
+                let response: Value =
+                    serde_json::from_value(serde_json::from_slice(response.stdout.as_slice())?)?;
+                println!("parsed response is {}", response.to_string());
+                Ok(response)
+            })
+        } else {
+            todo!()
+        }
+        // Box::pin(async { Ok(json!(["0x407d73d8a49eeb85d32cf465507dd71d507100c1"])) })
     }
 }
 
@@ -47,7 +89,10 @@ pub fn get_accounts() -> web3::error::Result<Vec<Vec<u8>>> {
     let accounts: web3::Result<Vec<Address>> = rt.block_on(accounts);
     println!("Accounts: {:?}", accounts);
 
-    Ok(accounts?.into_iter().map(|a: Address| a.as_bytes().to_vec()).collect())
+    Ok(accounts?
+        .into_iter()
+        .map(|a: Address| a.as_bytes().to_vec())
+        .collect())
 }
 
 #[marine]
@@ -61,7 +106,6 @@ extern "C" {
     pub fn curl_request(cmd: Vec<String>) -> MountedBinaryResult;
 }
 
-
 #[cfg(test)]
 mod tests {
     use web3::types::Address;
@@ -74,7 +118,9 @@ mod tests {
     )]
     fn get_accounts(rpc: marine_test_env::eth_rpc::ModuleInterface) {
         let accounts = rpc.call_get_accounts();
-        let addr: Address = "0x407d73d8a49eeb85d32cf465507dd71d507100c1".parse().unwrap();
+        let addr: Address = "0x407d73d8a49eeb85d32cf465507dd71d507100c1"
+            .parse()
+            .unwrap();
         assert_eq!(accounts, vec![addr.as_bytes().to_vec()]);
     }
 }
