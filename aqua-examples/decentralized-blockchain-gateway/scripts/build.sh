24fe1698d577572cf4ead6fea1902b5262a70aab
folex <0xdxdy@gmail.com>
2023-02-06 19:48:40 +0700

    chore: build eth-rpc

--- aqua-examples/decentralized-blockchain-gateway/scripts/build.sh
+++ aqua-examples/decentralized-blockchain-gateway/scripts/build.sh
@@ -8,33 +8,31 @@ rm -f artifacts/*.wasm
 cd wasm-modules
 
 cd curl-adapter
-cargo update --aggressive
 marine build --release
 cp target/wasm32-wasi/release/curl_adapter.wasm ../../artifacts/
 
 cd ../ipfs-adapter
-cargo update --aggressive
 marine build --release
 cp target/wasm32-wasi/release/ipfs_adapter.wasm ../../artifacts/
 
 cd ../multi-provider-query
-cargo update --aggressive
 marine build --release
 cp target/wasm32-wasi/release/multi_provider_query.wasm ../../artifacts/
 
 cd ../simple-quorum
-cargo update --aggressive
 marine build --release
 cp target/wasm32-wasi/release/simple_quorum.wasm ../../artifacts/
 
 cd ../ipfs-cli
-cargo update --aggressive
 marine build --release
 cp target/wasm32-wasi/release/ipfs_cli.wasm ../../artifacts/
 
 cd ../utilities
-cargo update --aggressive
 marine build --release
 cp target/wasm32-wasi/release/utilities.wasm ../../artifacts/
+
+cd ../eth-rpc
+marine build --release
+cp target/wasm32-wasi/release/eth-rpc.wasm ../../artifacts/
 
 cd ..
