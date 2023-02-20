f8c9c2a63f0e724c8fbc9e02708ceb80b6a395ad
boneyard93501 <4523011+boneyard93501@users.noreply.github.com>
2023-02-20 13:22:35 -0600

    update module name

--- marine-examples/records/facade/src/main.rs
+++ marine-examples/records/facade/src/main.rs
@@ -47,7 +47,7 @@ pub fn invoke() -> TestRecord {
 }
 
 #[marine]
-#[link(wasm_import_module = "records_effector")]
+#[link(wasm_import_module = "records_pure")]
 extern "C" {
     pub fn mutate_struct(test_record: TestRecord) -> TestRecord;
 }
