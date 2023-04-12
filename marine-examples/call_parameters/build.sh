7f0e9d495ea1ebe8d0535bb7f2ae0dc5d0a9d3b5
boneyard93501 <4523011+boneyard93501@users.noreply.github.com>
2023-04-11 19:36:40 -0500

    update to fluence cli

--- marine-examples/call_parameters/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit -o nounset -o pipefail
-
-cargo update --aggressive
-marine build --release
-
-mkdir -p artifacts
-rm -f artifacts/*.wasm
-cp target/wasm32-wasi/release/call_parameters.wasm artifacts/
