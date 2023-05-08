3567ad10468203edc8878bb28489faa2d576b697
Anatoly Laskaris <github_me@nahsi.dev>
2023-05-08 13:23:02 +0300

    Update CI

--- .github/download_marine.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -o pipefail -o errexit -o nounset
-set -x
-
-MARINE_RELEASE="https://github.com/fluencelabs/marine/releases/download/marine-v0.12.6/marine"
-curl -sS -L $MARINE_RELEASE -o /usr/local/bin/marine && chmod +x /usr/local/bin/marine
